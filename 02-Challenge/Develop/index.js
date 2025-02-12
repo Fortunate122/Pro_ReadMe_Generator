c00d6c7c6f41e2b6200035fd3fd2456628aa5536
Fortunate122 <davidsaldana122@gmail.com>
2025-02-11 21:23:27 -0800

    add js todos

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./generateMarkdown.js";
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your application:",
+    choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3-Clause", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test instructions:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("README.md created successfully!")
+  );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    const markdownContent = generateMarkdown(responses);
+    writeToFile("README.md", markdownContent);
+  });
+}
 
 // Function call to initialize app
 init();
--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,18 +1,81 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  }
+  const badges = {
+    MIT: "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)",
+    GPLv3: "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)",
+    Apache: "![License: Apache](https://img.shields.io/badge/License-Apache%202.0-red.svg)",
+    BSD: "![License: BSD](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)",
+  };
+  return badges[license] || "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  }
+  const links = {
+    MIT: "[MIT License](https://opensource.org/licenses/MIT)",
+    GPLv3: "[GPL v3 License](https://www.gnu.org/licenses/gpl-3.0)",
+    Apache: "[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)",
+    BSD: "[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)",
+  };
+  return links[license] || "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  }
+  return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${renderLicenseLink(license)}.`;
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For questions, you can reach out to me via:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 
 `;
 }
